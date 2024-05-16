80364b69e4bd0aa8dff542e4988224824502ea92
zhangjh <njhxzhangjihong@126.com>
2024-05-16 23:42:32 +0800

    update

--- start.py
+++ start.py
@@ -1,12 +1,13 @@
+
 import os
 import subprocess
-import shutil
+import sys
 
 def concatenate_and_extract(file_pattern, output_file):
     with open(output_file, 'wb') as outfile:
         for part in glob.glob(file_pattern):
             with open(part, 'rb') as infile:
-                shutil.copyfileobj(infile, outfile)
+                outfile.write(infile.read())
     subprocess.run(['tar', '-jxvf', output_file], check=True)
     os.remove(output_file)
 
@@ -15,31 +16,30 @@ def main(sample_audio, sample_text):
     split = "，。？！,.?!"
     work_dir = os.getcwd()
 
-    # Model integration steps
-    models_dir = os.path.join(work_dir, "GPT_SoVITS", "pretrained_models")
+    # 模型整合
+    models_dir = os.path.join(work_dir, 'GPT_SoVITS', 'pretrained_models')
     os.chdir(models_dir)
-    
-    concatenate_and_extract('s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.part.*', 's1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2')
-    subprocess.run(['tar', '-jxvf', 's2D488k.pth.bz2'], check=True)
+    concatenate_and_extract('s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2.part.*', 
+                            's1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2')
+    subprocess.run(['tar', '-jxvf', 's2G488k.pth.bz2'], check=True)
 
-    os.chdir(os.path.join(models_dir, "chinese-hubert-base"))
+    os.chdir(os.path.join(models_dir, 'chinese-hubert-base'))
     concatenate_and_extract('pytorch_model.bin.bz2.part.*', 'pytorch_model.bin.bz2')
     subprocess.run(['tar', '-jxvf', 'pytorch_model.bin.bz2'], check=True)
     os.remove('pytorch_model.bin.bz2')
 
-    os.chdir(os.path.join(models_dir, "../chinese-roberta-wwm-ext-large"))
+    os.chdir(os.path.join(models_dir, '../chinese-roberta-wwm-ext-large'))
     concatenate_and_extract('pytorch_model.bin.bz2.part.*', 'pytorch_model.bin.bz2')
     subprocess.run(['tar', '-jxvf', 'pytorch_model.bin.bz2'], check=True)
     os.remove('pytorch_model.bin.bz2')
 
-    # Running the API script
-    api_script_path = os.path.join(work_dir, "api.py")
-    subprocess.run(['python3', api_script_path, '-dr', sample_audio, '-dt', sample_text, '-dl', lang, '-cp', split], check=True)
+    # 执行API
+    api_script = os.path.join(work_dir, 'api.py')
+    args = ['-dl', lang, '-cp', split]
+    if sample_audio or sample_text:
+        args.extend(['-dr', sample_audio, '-dt', sample_text])
+    subprocess.run(['python3', api_script] + args, check=True)
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) != 3:
-        print("Usage: python script_name.py <sample_audio> <sample_text>")
-        sys.exit(1)
-    main(sys.argv[1], sys.argv[2])
-
+    main(sys.argv[1] if len(sys.argv) > 1 else "", 
+         sys.argv[2] if len(sys.argv) > 2 else "")
--- start.sh
+++ start.sh
@@ -11,9 +11,9 @@ work_dir=`pwd`
 #conda activate GPTSoVits
 ## 模型整合
 cd ${work_dir}/GPT_SoVITS/pretrained_models
-cat s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.part.* > s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2
+cat s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2.part.* > s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2
 tar -jxvf s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2 && rm s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2
-tar -jxvf s2D488k.pth.bz2
+tar -jxvf s2G488k.pth.bz2
 
 cd chinese-hubert-base/
 cat pytorch_model.bin.bz2.part.* > pytorch_model.bin.bz2
@@ -23,4 +23,8 @@ cd ../chinese-roberta-wwm-ext-large
 cat pytorch_model.bin.bz2.part.* > pytorch_model.bin.bz2
 tar -jxvf pytorch_model.bin.bz2 && rm pytorch_model.bin.bz2
 
-python3 ${work_dir}/api.py -dr "${sample_audio}" -dt "${sample_text}" -dl "${lang}" -cp "${split}"
+if [[ "${sample_audio}" != "" || "${sample_text}" != "" ]];then
+  python3 ${work_dir}/api.py -dr "${sample_audio}" -dt "${sample_text}" -dl "${lang}" -cp "${split}"
+else
+  python3 ${work_dir}/api.py -dl "${lang}" -cp "${split}"
+fi
