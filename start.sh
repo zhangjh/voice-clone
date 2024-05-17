fa9ecbae37f6c4af6d9e8a0d7fd6a276dabc6200
zhangjh <njhxzhangjihong@126.com>
2024-05-17 23:19:55 +0800

    add

--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 #### author: zhangjihong
 #### desc: 启动ai服务
 
-sample_audio="$1"
-sample_text="$2"
 lang="zh"
 split="，。？！,.?!"
 work_dir=`pwd`
-#conda create -n GPTSoVits python=3.9
-#conda activate GPTSoVits
+
+cp .condarc ~/
+conda create -n GPTSoVits python=3.9 -y
+conda activate GPTSoVits
 ## 模型整合
 cd ${work_dir}/GPT_SoVITS/pretrained_models
 cat s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2.part.* > s1bert25hz-2kh-longer-epoch=68e-step=50232.ckpt.bz2
@@ -25,8 +25,4 @@ tar -jxvf pytorch_model.bin.bz2 && rm pytorch_model.bin.bz2
 
 cd ${work_dir}
 
-if [[ "${sample_audio}" != "" || "${sample_text}" != "" ]];then
-  python3 ${work_dir}/api.py -dr "${sample_audio}" -dt "${sample_text}" -dl "${lang}" -cp "${split}"
-else
-  python3 ${work_dir}/api.py -dl "${lang}" -cp "${split}"
-fi
+python3 ${work_dir}/api.py -dl "${lang}" -cp "${split}"
